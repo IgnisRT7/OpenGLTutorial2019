3761dd02a2e005667a8ae3076417c97ee018fa21
IgnisRT7 <35602066+IgnisRT7@users.noreply.github.com>
2019-05-22 12:00:04 +0900

    テクスチャクラスの追加

---
+++ src/Texture.h
@@ -0,0 +1,42 @@
+#pragma once
+/**
+*	@Texture.h
+*/
+#include <GL/glew.h>
+#include <memory>
+
+/**
+*	講義資料との違いまとめ
+*
+*	(Image2D, Image2DPtr) == (Texture, TexturePtr)
+*	Image2D**Create(const char* path) == Texture::LoadFromFile(const char*)
+*	関数追加 : Texture::Reset(LGuint)
+*/
+
+class Texture;
+using TexturePtr = std::shared_ptr<Texture>;	///< テクスチャポインタ
+
+/**
+*	テクスチャクラス
+*/
+class Texture {
+public:
+	
+	static TexturePtr Create(int width, int height, GLenum iformat, GLenum format, const void* data);
+	static TexturePtr LoadFromFile(const char*);
+	GLuint Id() const { return texId; }
+	GLsizei Width() const { return width; }
+	GLsizei Height() const { return height; }
+	void Reset(GLuint texId);
+
+private:
+
+	Texture() = default;
+	~Texture();
+	Texture(const Texture&) = delete;
+	Texture& operator=(const Texture&) = delete;
+
+	GLuint texId = 0;
+	int width = 0;
+	int height = 0;
+};
