d5d5f9c946125c5207a0b4107f4255d7881325d8
IgnisRT7 <35602066+IgnisRT7@users.noreply.github.com>
2019-06-26 18:35:46 +0900

    スプライトクラスの設定

--- src/TitleScene.cpp
+++ src/TitleScene.cpp
@@ -6,6 +6,39 @@
 #include "GLFWEW.h"
 
 /**
+*	シーンを初期化する
+*
+*	@param true		初期化成功
+*	@param false	初期化失敗
+*
+*/
+bool TitleScene::Initialize() {
+
+	spriteRenderer.Init(1000, "res/Sprite.vert", "res/Sprite.frag");
+	sprites.reserve(100);
+	Sprite spr(Texture::LoadFromFile("res/Player.dds"));
+	spr.Scale(glm::vec2(2));
+	sprites.push_back(spr);
+
+	return true;
+}
+
+/**
+*	シーンを更新する
+*	
+*	@param deltaTime	前回の更新からの経過時間(秒)
+*/
+void TitleScene::Update(float deltaTime) {
+
+	spriteRenderer.BeginUpdate();
+	for (const Sprite& e : sprites) {
+		spriteRenderer.AddVertices(e);
+	}
+	spriteRenderer.EndUpdate();
+}
+
+
+/**
 *	プレイヤーの入力を処理する
 */
 void TitleScene::ProcessInput() {
@@ -16,8 +49,16 @@ void TitleScene::ProcessInput() {
 
 		SceneStack::Instance().Replace(std::make_shared<MainGameScene>());
 	}
+}
 
+/**
+*	シーンを描画する
+*/
+void TitleScene::Render() {
 
+	const GLFWEW::Window& window = GLFWEW::Window::Instance();
+	const glm::vec2 screenSize(window.Width(), window.Height());
+	spriteRenderer.Draw(screenSize);
 }
 
 
--- src/TitleScene.h
+++ src/TitleScene.h
@@ -4,6 +4,8 @@
 #ifndef TITLESCENE_H_INCLUDE
 #define TITLESCENE_H_INCLUDE
 #include "Scene.h"
+#include <vector>
+#include "Sprite.h"
 
 /**
 *	ƒ^ƒCƒgƒ‹‰æ–Ê
@@ -14,11 +16,16 @@ public:
 	TitleScene() :Scene("TitleScene") {}
 	virtual ~TitleScene() = default;
 
-	virtual bool Initialize() override { return true; }
+	virtual bool Initialize() override;
 	virtual void ProcessInput() override;
-	virtual void Update(float) override {}
-	virtual void Render() override {}
+	virtual void Update(float) override;
+	virtual void Render() override;
 	virtual void Finalize() override {}
+
+private:
+
+	std::vector<Sprite> sprites;
+	SpriteRenderer spriteRenderer;
 };
 
 
