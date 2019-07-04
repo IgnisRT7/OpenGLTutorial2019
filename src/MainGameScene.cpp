caf247c36ae2bc35ec7de56a0678e5811d8ae481
IgnisRT7 <35602066+IgnisRT7@users.noreply.github.com>
2019-07-04 12:14:43 +0900

    クラスのメンバ関数の実装

--- src/MainGameScene.cpp
+++ src/MainGameScene.cpp
@@ -5,6 +5,27 @@
 #include "MainGameScene.h"
 #include "StatusScene.h"
 #include "GameOverScene.h"
+#include "GLFWEW.h"
+#include <glm/gtc/matrix_transform.hpp>
+
+/**
+*	初期化処理
+*/
+bool MainGameScene::Initialize(){
+
+	spriteRenderer.Init(1000, "res/Sprite.vert", "res/Sprite.frag");
+	fontRenderer.Init(1000);
+	sprites.reserve(100);
+
+	fontRenderer.LoadFromFile("Res/font.fnt");
+	Sprite spr(Texture::Image2D::Create("res/TitleBg.tga"));
+	spr.Scale(glm::vec2(2));
+	sprites.push_back(spr);
+
+	meshBuffer.Init(1'000'000 * sizeof(Mesh::Vertex), 3'000'000 * sizeof(GLushort));
+
+	return true;
+}
 
 /**
 *	プレイヤーの入力を処理する
@@ -13,10 +34,41 @@ void MainGameScene::ProcessInput() {
 
 	if (!frag) {
 		frag = true;
-		SceneStack::Instance().Push(std::make_shared<StatusScene>());
+	//	SceneStack::Instance().Push(std::make_shared<StatusScene>());
 	}
 	else {
 
-		SceneStack::Instance().Replace(std::make_shared<GameOverScene>());
+	//	SceneStack::Instance().Replace(std::make_shared<GameOverScene>());
 	}
+}
+
+/**
+*	更新処理
+*
+*	@param deltaTime	経過時間
+*/
+void MainGameScene::Update(float deltaTime){
+
+
+}
+
+/**
+*	描画処理
+*/
+void MainGameScene::Render(){
+
+	const GLFWEW::Window& window = GLFWEW::Window::Instance();
+	const glm::vec2 screenSize(window.Width(), window.Height());
+	spriteRenderer.Draw(screenSize);
+	fontRenderer.Draw(screenSize);
+
+	const glm::vec3 cameraPos(-5, 3, 5);
+	const glm::vec3 targetPos(0, 0, 0);
+	const glm::mat4 matView = glm::lookAt(cameraPos, targetPos, glm::vec3(0, 1, 0));
+
+	const float aspectRatio = static_cast<float>(window.Width() / static_cast<float>(window.Height()));
+	const glm::mat4 matProj = glm::perspective(glm::radians(30.0f), aspectRatio, 1.0f, 1000.0f);
+	const glm::mat4 matModel(1);
+	Mesh::Draw(meshBuffer.GetFile("Cube"), matProj*matView, matModel);
+
 }
--- src/MainGameScene.h
+++ src/MainGameScene.h
@@ -4,6 +4,10 @@
 #ifndef MAINGAMESCENE_H_INCLUDE
 #define MAINGAMESCENE_H_INCLUDE
 #include "Scene.h"
+#include "Sprite.h"
+#include "Font.h"
+#include "Mesh.h"
+#include <vector>
 
 /**
 *	ÉÅÉCÉìÉQÅ[ÉÄâÊñ 
@@ -14,14 +18,19 @@ public:
 	MainGameScene() : Scene("MainGameScene") {}
 	virtual ~MainGameScene() = default;
 
-	virtual bool Initialize() override { return true;}
+	virtual bool Initialize() override;
 	virtual void ProcessInput() override;
-	virtual void Update(float) override {}
-	virtual void Render() override {}
+	virtual void Update(float) override;
+	virtual void Render() override;
 	virtual void Finalize() override {}
 
 private:
+
 	bool frag = false;
+	SpriteRenderer spriteRenderer;
+	FontRenderer fontRenderer;
+	Mesh::Buffer meshBuffer;
+	std::vector<Sprite> sprites;
 };
 
 #endif //MAINGAMESCENE_H_INCLUDE
