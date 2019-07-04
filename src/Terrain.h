5786b992c8d927a6fb5b0cd936a16e19ce28403b
IgnisRT7 <35602066+IgnisRT7@users.noreply.github.com>
2019-07-04 20:38:01 +0900

    ハイトマップによる地形生成クラスの実装

---
+++ src/Terrain.h
@@ -0,0 +1,44 @@
+/**
+*	@file Terrain.h
+*/
+#ifndef TERRAIN_H_INCLUDE
+#define TERRAIN_H_INCLUDE
+
+#include "Mesh.h"
+#include <glm/glm.hpp>
+#include <string>
+#include <vector>
+
+namespace Terrain {
+
+	/**
+	*	高さマップ
+	*
+	*	1, LoadFromFile()で画像ファイルから高さ情報を読み込む
+	*	2, CreateMesh()で読み込んだ高さ情報から地形メッシュを作成する
+	*	3, ある地点の高さを調べるにはHeight()を使う
+	*/
+	class HeightMap {
+	public:
+
+		HeightMap() = default;
+		~HeightMap() = default;
+
+		bool LoadFromFile(const char* path, float scale, float baseLevel);
+		float Height(const glm::vec3& pos) const;
+		const glm::ivec2 Size() const;
+		bool CreateMesh(Mesh::Buffer& meshBuffer, const char* meshName, const char* texName = nullptr) const;
+
+	private:
+
+		std::string name;
+		glm::ivec2 size = glm::ivec2(0);			///< もとになった画像ふぁぃるめい
+		std::vector<float> heights;					///< ハイトマップの大きさ
+
+		glm::vec3 CalcNormal(int x, int z) const;	///< 高さデータ
+	};
+
+
+} // namespace Terrain
+
+#endif
